aa86e76ba68477bd07c2e54a561729a2749c68a9
Charles <60122616+Xhether@users.noreply.github.com>
2024-06-22 13:20:03 -0700

    Initial Commit

---
+++ Berkeley_AI_Hackathon/Berkeley_AI_HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Berkeley_AI_HackathonApp.swift
+//  Berkeley_AI_Hackathon
+//
+//  Created by Charles Liggins on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Berkeley_AI_HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
